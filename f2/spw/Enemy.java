61ced03e77b8dc2fb5c007b4f862956cd8780c8b
Ruslan417 <cone.dudida@gmail.com>
2015-03-30 22:37:25 +0700

    *01*

--- f2/spw/Enemy.java
+++ f2/spw/Enemy.java
@@ -10,7 +10,7 @@ public class Enemy extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 20;
+	private int step = 50;
 	private boolean alive = true;
 	private Image pic;
 	public Enemy(int x, int y) {
