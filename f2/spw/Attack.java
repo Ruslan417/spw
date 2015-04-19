ba4fa97bee94207ef0cf64e84fe8fda7347ad140
Ruslan417 <cone.dudida@gmail.com>
2015-04-19 12:07:16 +0700

    *05* Edit Attack before Attack error now it's work

--- f2/spw/Attack.java
+++ f2/spw/Attack.java
@@ -4,9 +4,9 @@ import java.awt.Graphics2D;
 
 public class Attack extends Sprite{
 	public static final int Y_TO_FADE = 400;
-	public static final int Y_TO_DIE = 600;
+	public static final int Y_TO_DIE = 0;
 	
-	private int step = 8;
+	private int step = 10;
 	private boolean alive = true;
 	private boolean hit = false;
 	
@@ -29,26 +29,13 @@ public class Attack extends Sprite{
 	}
 
 	public void proceed(){
-		y += step;
-		if(y > Y_TO_DIE){
+		y -= step;
+		if(y < Y_TO_DIE){
 			alive = false;
 		}
 	}
 	
 	public boolean isAlive(){
 		return alive;
-	}
-	
-	public void hited(){
-		this.alive = false;
-	}
-
-	public boolean isHit() {
-	 return hit ;
-	}
-	
-	public void getHit(){
-		this.hit = true;
-		this.alive = false;
 	}
 }
