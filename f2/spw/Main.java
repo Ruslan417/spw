b0cd4a23c8479b9c5ff439c9c793d2770b29e43a
Ruslan417 <cone.dudida@gmail.com>
2015-05-05 19:06:48 +0700

    *09* Create Menu bar

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -11,6 +11,30 @@ public class Main {
 		frame.setSize(500, 800);
 		frame.getContentPane().setLayout(new BorderLayout());
 		
+		//09
+		Font fn= new Font("Courier New", Font.BOLD,20);
+                JMenuBar menubar = new JMenuBar();
+                JMenu menuMain = new JMenu("Main");
+                JMenu menuAbout = new JMenu("About");
+                JMenuItem menuRestart =new JMenuItem("Restart");
+                JMenuItem menuExit = new JMenuItem("Exit");
+                JMenuItem menuStop = new JMenuItem("Stop");
+                JMenuItem menuCredit = new JMenuItem("Credit");      
+                menuMain.setFont(fn);
+                menuAbout.setFont(fn);
+                menuRestart.setFont(fn);
+                menuExit.setFont(fn);
+                menuStop.setFont(fn);
+                menuCredit.setFont(fn);
+                menuMain.add(menuRestart);
+                menuMain.add(menuStop);
+                menuMain.add(menuExit);
+                menuAbout.add(menuCredit);
+                menubar.add(menuMain);
+                menubar.add(menuAbout);
+                frame.setJMenuBar(menubar);
+		//end 09
+		
 		SpaceShip v = new SpaceShip(180, 550, 20, 20);
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
