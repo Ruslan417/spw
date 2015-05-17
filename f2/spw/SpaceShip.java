f66d7c576eb81b8019bd1537e885ef49ad86746f
Ruslan417 <cone.dudida@gmail.com>
2015-05-17 13:34:43 +0700

    ไม่ได้แก้เพิ่มครับ ครั้งสุดท้ายคือ *09*
    คือไฟล์เก่าติด error ตอนนี้ แก้มันสามารถทำงานได้ครับ

--- f2/spw/SpaceShip.java
+++ f2/spw/SpaceShip.java
@@ -1,37 +1,66 @@
-package f2.spw;
+//package f2.spw;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
 
+
+import java.awt.Toolkit;
+import java.awt.Image;
+
 public class SpaceShip extends Sprite{
 
-	int step = 15;
+	int hp = 5;
+	boolean alive = true;
+	int step = 8;
 	
 	public SpaceShip(int x, int y, int width, int height) {
-		super(x, y, width, height);
+		super(x, y, 50, 40);
 		
 	}
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.setColor(Color.WHITE);
-		g.fillRect(x, y, width, height);
+		/*g.setColor(Color.GREEN);
+		g.fillRect(x, y, width, height);*/
+		Image pic = Toolkit.getDefaultToolkit().getImage("Space.png");
+        g.drawImage(pic, x, y, width, height, null);
 		
 	}
 
-	public void move(int direction){
-		x += (step * direction);
-		if(x < 0)
-			x = 0;
-		if(x > 500 - width)
-			x = 500 - width;
+	public void move(int directionX, int directionY){
+		if(directionX != 0 && directionY == 0 ){
+			x += (step * directionX);
+			if(x < 0)
+				x = 0;
+			if(x > 400 - width)
+				x = 400 - width;
+		}
+		if(directionX == 0 && directionY != 0 ){
+			y += (step * directionY);
+			if(y < 0)
+				y = 0;
+			if(y > 600 - height)
+				y = 600	- height;
+		}	
+	}
+	
+	
+	public void hit() {
+		//hp = 100;
+		hp--;
+		if(hp <= 0) {
+			alive = false;
+			hp = 0;
 			
-			
-		y += (step * direction);
-		if(y < 0)
-			y = 0;
-		if(y > 800 - height)
-			y = 800 - height;
+			}		
+	}
+
+	public int gethp() {
+		return hp;
+	}
+	
+	public boolean isAlive(){
+		return alive;
 	}
 
 }
