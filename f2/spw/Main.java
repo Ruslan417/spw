40b49378cd22a6dfcdcfa2ab05c364551ff55fba
Ruslan417 <cone.dudida@gmail.com>
2015-05-17 13:34:07 +0700

    ไม่ได้แก้เพิ่มครับ ครั้งสุดท้ายคือ *09*
    คือไฟล์เก่าติด error ตอนนี้ แก้มันสามารถทำงานได้ครับ

--- f2/spw/Main.java
+++ f2/spw/Main.java
@@ -1,43 +1,52 @@
-package f2.spw;
+//package f2.spw;
 
 import java.awt.BorderLayout;
-
 import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
 public class Main {
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Space War");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 800);
+		frame.setSize(400, 650);
 		frame.getContentPane().setLayout(new BorderLayout());
 		
-		//09
-		Font fn= new Font("Courier New", Font.BOLD,20);
+
                 JMenuBar menubar = new JMenuBar();
-                JMenu menuMain = new JMenu("Main");
-                JMenu menuAbout = new JMenu("About");
+                JMenu menuMain = new JMenu("Menu");
+                JMenu menuAbout = new JMenu("Exit");
                 JMenuItem menuRestart =new JMenuItem("Restart");
                 JMenuItem menuExit = new JMenuItem("Exit");
-                JMenuItem menuStop = new JMenuItem("Stop");
-                JMenuItem menuCredit = new JMenuItem("Credit");      
-                menuMain.setFont(fn);
-                menuAbout.setFont(fn);
-                menuRestart.setFont(fn);
-                menuExit.setFont(fn);
-                menuStop.setFont(fn);
-                menuCredit.setFont(fn);
+                JMenuItem menuStop = new JMenuItem("Pause");
+                //JMenuItem menuCredit = new JMenuItem("Credit");      
+                //menuMain.setFont(fn);
+                //menuAbout.setFont(fn);
+                //menuRestart.setFont(fn);
+                //menuExit.setFont(fn);
+                //menuStop.setFont(fn);
+                //menuCredit.setFont(fn);
                 menuMain.add(menuRestart);
                 menuMain.add(menuStop);
-                menuMain.add(menuExit);
-                menuAbout.add(menuCredit);
+                //menuMain.add(menuExit);
+                menuAbout.add(menuExit);
                 menubar.add(menuMain);
                 menubar.add(menuAbout);
                 frame.setJMenuBar(menubar);
-		//end 09
+                
 		
 		SpaceShip v = new SpaceShip(180, 550, 20, 20);
 		GamePanel gp = new GamePanel();
 		GameEngine engine = new GameEngine(gp, v);
+		
+		MenuListener menuListener = new MenuListener(menuExit,menuRestart,menuStop,engine);
+                menuExit.addActionListener(menuListener);
+                //menuCredit.addActionListener(menuListener);
+                menuRestart.addActionListener(menuListener);
+                menuStop.addActionListener(menuListener);
+
+				
 		frame.addKeyListener(engine);
 		frame.getContentPane().add(gp, BorderLayout.CENTER);
 		frame.setVisible(true);
