afbbfc6ea71fac7e9f380aa1dd41d9d8bcf8b1ff
Ruslan417 <cone.dudida@gmail.com>
2015-05-17 13:33:21 +0700

    ไม่ได้แก้เพิ่มครับ ครั้งสุดท้ายคือ *09*
    คือไฟล์เก่าติด error ตอนนี้ แก้มันสามารถทำงานได้ครับ

--- f2/spw/Item.java
+++ f2/spw/Item.java
@@ -1,24 +1,25 @@
-package f2.spw;
+//package f2.spw;
 
 import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics2D;
+
+import java.awt.Toolkit;
 import java.awt.Image;
-import javax.swing.ImageIcon;
 
 public class Item extends Sprite{
 	public static final int Y_TO_FADE = 400;
 	public static final int Y_TO_DIE = 600;
 	
-	private int step = 50;
+	private int step = 5;
 	private boolean alive = true;
-	private Image pic;
-	private int points = 0;
+	
+	
 	public Item(int x, int y) {
-		super(x, y, 5, 10);
+		super(x, y, 30, 40);
 		
 	}
-	
+
 	@Override
 	public void draw(Graphics2D g) {
 		if(y < Y_TO_FADE)
@@ -28,20 +29,23 @@ public class Item extends Sprite{
 					(float)(Y_TO_DIE - y)/(Y_TO_DIE - Y_TO_FADE)));
 		}
 		
-		if((int)(Math.random()*2) == 0)	
-			g.setColor(Color.PINK);			
-		else
-			g.setColor(Color.BLUE);
-		g.fillRect(x, y, width, height);
-	
+		//g.setColor(Color.RED);
+		//g.fillRect(x, y, width, height);
+		
+		
+		Image pic = Toolkit.getDefaultToolkit().getImage("item.gif"); 
+		g.drawImage(pic, x, y, width, height, null);
 		
 	}
 
 	public void proceed(){
 		y += step;
 		if(y > Y_TO_DIE){
-			points += 50;
+			alive = false;
 		}
+	}
+	public void hit(){
+		alive = false;
 	}
 	
 	public boolean isAlive(){
